8b75f1840826b725a669464027c9a71179d487fe
gabriel-carvalhos <gabrielcarvalhos.snt@gmail.com>
2024-02-17 19:13:37 -0300

    texto intro responsivo

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
 const intro = document.querySelector('.intro')
 const tl = gsap.timeline()
+
+let introConfig
+if (window.matchMedia("(min-width: 768px)").matches) {
+    // MAIOR
+    introConfig = { fs: '85px', w: '350px', h: '110px', t: '33px' }
+} else {
+    // MENOR
+    introConfig = { fs: '40px', w: '176.203px', h: '60px', t: '24px' }
+}
+
 tl.to(".intro", {
-    fontSize: "85px",
-    width: "350px",
-    height: "110px",
-    top: "33px",
+    fontSize: introConfig.fs,
+    width: introConfig.w,
+    height: introConfig.h,
+    top: introConfig.t,
     left: "20px",
     duration: 2,
     delay: 5,
@@ -14,14 +24,14 @@ tl.to(".intro", {
 
     ease: "power4.inOut",
     onComplete() {
-        intro.classList.add('md:text-[85px]', '[font-size:9.25vw]')
+        intro.classList.add('md:intro-text', 'intro-text')
         
+        intro.style.fontSize = ''
         intro.style.width = 'fit-content'
         intro.style.height = 'fit-content'
         intro.style.position = 'relative'
         intro.style.top = '0'
         intro.style.left = '0'
-        intro.style.fontSize = ''
     }
 })
 
